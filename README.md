ed25996929290a20d076b0211f4d6327757f7434
К Аманбаев <kamanbaev@rp.lan>
2023-12-07 00:24:33 +0600

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+## Books Catalog
+
+#### how to run
+```sh
+# clone project
+# create and activate venv
+pip install -r requirements.txt
+# create an .env file (like .env_example)
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py runserver
+```
---
+++ users/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from users.models import User
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
